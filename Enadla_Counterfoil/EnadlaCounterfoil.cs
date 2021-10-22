31538d1fa7365868e04223f988412ac9f511e84c
Soworksl <Jimy.waner11@gmail.com>
2021-10-21 23:43:44 -0400

    Initial commit

---
+++ Enadla_Counterfoil/EnadlaCounterfoil.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System.IO;
+
+namespace Enadla_Counterfoil
+{
+    public class EnadlaCounterfoil
+    {
+        private SQLiteConnection sourceDb;
+
+        public EnadlaCounterfoil(string path)
+        {
+            bool isNewCounterfoil = File.Exists(path);
+            sourceDb = new SQLiteConnection(path);
+            if (isNewCounterfoil)
+            {
+                //Crear todas las tablas y poner la version de la base de dato
+            }
+        }
+    }
+}
